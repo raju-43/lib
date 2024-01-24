0eda23ec431352f9fb9c83c8196055cbfac93b8f
raju-43 <97400343+raju-43@users.noreply.github.com>
2024-01-24 12:51:30 +0530

    Update Book.java

--- Book.java
+++ Book.java
@@ -1,39 +1,89 @@
-class Book {
-    public static void main(String[] args) {
-     Book[] obj = new Book[10];
-  
-    
-    obj[0] = new Book(10,"Member1");  
-obj[1] = new Book(20,"Member2");  
-obj[2] = new Book(29,"Member3");  
-obj[3] = new Book(11,"Member4");  
-obj[4] = new Book(30,"Member5");  
- obj[5] = new Book(10,"Member6");  
-obj[6] = new Book(21,"Member7");  
-obj[7] = new Book(22,"Member8");  
-obj[8] = new Book(15,"Member9");  
-obj[9] = new Book(25,"Member10");  
+import java.util.scanner;
 
-System.out.println("Book Object 1:");  
-obj[0].display();  
-System.out.println("Book Object 2:");  
-obj[1].display();  
-System.out.println("Book Object 3:");  
-obj[2].display();  
-System.out.println("Book Object 4:");  
-obj[3].display();  
-System.out.println("Book Object 5:");  
-obj[4].display();  
-System.out.println("Book Object 6:");  
-obj[5].display();  
-System.out.println("Book Object 7:");  
-obj[6].display();  
-System.out.println("Book Object 8:");  
-obj[7].display();  
-System.out.println("Book Object 9:");  
-obj[8].display();  
-System.out.println("Book Object 5:");  
-obj[9].display();  
-    
+
+
+class Member {
+    String name;
+    ArrayList<Book> books;
+
+    public Member(String name) {
+        this.name = name;
+        this.books = new ArrayList<>();
     }
 }
+
+class Book {
+    String name;
+    String checkoutDate;
+    String returnDate;
+
+    public Book(String name, String checkoutDate, String returnDate) {
+        this.name = name;
+        this.checkoutDate = checkoutDate;
+        this.returnDate = returnDate;
+    }
+}
+
+public class Library {
+    public static void main(String[] args) {
+        int numberOfMembers = 10;
+
+        Member[] members = new Member[numberOfMembers];
+
+   
+        for (int i = 0; i < numberOfMembers; i++) {
+            members[i] = new Member("Member" + (i + 1));
+            int numberOfBooks = new Random().nextInt(21) + 10; 
+           
+            for (int j = 0; j < numberOfBooks; j++) {
+                String bookName = "Book" + (j + 1);
+                String checkoutDate = generateRandomDate();
+                String returnDate = generateRandomDate();
+                members[i].books.add(new Book(bookName, checkoutDate, returnDate));
+            }
+        }
+
+        
+        double totalDays = 0;
+        int totalBooks = 0;
+
+        for (Member member : members) {
+            for (Book book : member.books) {
+                totalDays += calculateDaysDifference(book.checkoutDate, book.returnDate);
+                totalBooks++;
+            }
+        }
+
+        double averageDays = totalDays / totalBooks;
+        System.out.println("Average number of days each member keeps the book: " + averageDays);
+    }
+
+    
+    private static String generateRandomDate() {
+        int day = new Random().nextInt(28) + 1; 
+        int month = new Random().nextInt(12) + 1; 
+        int year = new Random().nextInt(5) + 2020; 
+
+        return String.format("%02d-%02d-%04d", day, month, year);
+    }
+
+    
+    private static int calculateDaysDifference(String startDate, String endDate) {
+        
+
+        String[] startParts = startDate.split("-");
+        String[] endParts = endDate.split("-");
+
+        int startDay = Integer.parseInt(startParts[0]);
+        int startMonth = Integer.parseInt(startParts[1]);
+        int startYear = Integer.parseInt(startParts[2]);
+
+        int endDay = Integer.parseInt(endParts[0]);
+        int endMonth = Integer.parseInt(endParts[1]);
+        int endYear = Integer.parseInt(endParts[2]);
+
+        return Math.abs((endYear - startYear) * 365 + (endMonth - startMonth) * 30 + (endDay - startDay));
+    }
+}
+
+
